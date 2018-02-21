52ddc5f423ef93ea8cbd6bfcad7658ac15ca67b0
unknown <uenvymenow@gmail.com>
2018-02-21 15:22:21 -0500

    Added skeleton layout for app for created the submitAnswers method w/o any functionality yet

--- app/src/main/java/com/example/android/quizapplication/MainActivity.java
+++ app/src/main/java/com/example/android/quizapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.quizapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // called from the onClick for the submit_answers_button in activity_main.xml
+    public void submitAnswers(View view){
+        // Calls the check answers method
+
+        // Calls the calculate answers method
+
+        // Calls the display method
     }
 }
