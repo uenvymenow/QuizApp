25b45a9abed7068242417e215534f6e1ae8869b0
unknown <uenvymenow@gmail.com>
2018-02-22 00:52:06 -0500

    Added test radio buttons for one question and created the onClick event for the submit button and calculated final score and showed the score in a toast message

--- app/src/main/java/com/example/android/quizapplication/MainActivity.java
+++ app/src/main/java/com/example/android/quizapplication/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.android.quizapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    int percentCorrect = 0;
+    int totalQuestions = 1;
+    int correctAnswers = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,11 +19,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // called from the onClick for the submit_answers_button in activity_main.xml
-    public void submitAnswers(View view){
-        // Calls the check answers method
-
-        // Calls the calculate answers method
-
+    public void submitAnswers(View view) {
         // Calls the display method
+        displayMessage(percentCorrect);
+    }
+
+    public void question1RadioButton(View view) {
+        // Import radio buttons for question 1
+        RadioButton questionOneCorrectRB = findViewById(R.id.questionOneCorrect);
+        RadioButton questionOneIncorrectRB = findViewById(R.id.questionOneIncorrect);
+
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.questionOneCorrect:
+                if (questionOneCorrectRB.isChecked())
+                    correctAnswers += 1;
+                break;
+            case R.id.questionOneIncorrect:
+                if (questionOneIncorrectRB.isChecked()) {
+                    break;
+                }
+        }
+    }
+
+    public int calculateFinalScore(int correctAnswers, int totalQuestions) {
+        double finalScore = (double) correctAnswers / (double) totalQuestions;
+        int percentCorrect = (int) finalScore * 100;
+        return percentCorrect;
+    }
+
+    public void displayMessage(int finalPercentage) {
+        Toast.makeText(this, "Your final score is " + calculateFinalScore(correctAnswers, totalQuestions) + "%", Toast.LENGTH_SHORT).show();
+        resetScores();
+    }
+
+    public void resetScores() {
+        percentCorrect = 0;
+        totalQuestions = 1;
+        correctAnswers = 0;
     }
 }
